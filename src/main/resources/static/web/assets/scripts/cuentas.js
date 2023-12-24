c4f281efae749bf42010793e28761cf0cadce9e4
Pablo Exequiel Gimenez <142545389+PabloExeQGimenez@users.noreply.github.com>
2023-12-24 02:51:12 -0300

    Accounts muestra datos personales y cuentas

---
+++ src/main/resources/static/web/assets/scripts/cuentas.js
@@ -0,0 +1,64 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            clienteMelba: "",
+            cuentasMelba:"",
+            visibleDatosPersonales: false,
+            visibleCuentas: false,
+            name:"",
+            lastName:"",
+            email:"",
+        };
+    },
+
+    created() {
+
+        this.cliente1()
+
+
+    },
+    methods: {
+
+        cliente1() {
+            axios
+                .get("/api/clientes/1")
+                .then(respuesta => {
+                    this.clienteMelba = respuesta.data
+                    console.log(this.clienteMelba)
+                    this.cuentasMelba = this.clienteMelba.cuentas
+                    console.log(this.cuentasMelba)
+
+                    this.name = this.clienteMelba.nombre
+                    this.lastName = this.clienteMelba.apellido
+                    this.email = this.clienteMelba.correo
+                })
+                .catch(error => {
+                    console.error('Error al obtener clientes:', error);
+                });
+        },
+
+        mostrarDatosPersonales(){
+            this.visibleDatosPersonales = true
+            this.visibleCuentas = false
+        },
+        mostrarCuentas(){
+            this.visibleCuentas= true
+            this.visibleDatosPersonales = false
+
+        },
+
+    logout() {
+        axios
+            .post("http://localhost:8080/api/logout")
+            .then((response) => {
+                console.log("signed out!!!");
+                location.href = "http://localhost:8080/web/index.html";
+            })
+            .catch((error) => console.log(error));
+    }
+
+}
+
+}).mount('#app');
