42da4c6770e3af2f7833e9650ab981bcefcc74a1
Pablo Exequiel Gimenez <142545389+PabloExeQGimenez@users.noreply.github.com>
2023-12-24 15:03:37 -0300

    Entidad Transactions linsta

--- src/main/resources/static/web/assets/scripts/cuentas.js
+++ src/main/resources/static/web/assets/scripts/cuentas.js
@@ -1,76 +1,69 @@
-const { createApp } = Vue
+const { createApp } = Vue;
 
 createApp({
     data() {
         return {
             clienteMelba: "",
-            cuentasMelba:"",
+            cuentasMelba: "",
             visibleDatosPersonales: false,
             visibleCuentas: false,
-            name:"",
-            lastName:"",
-            email:"",
+            name: "",
+            lastName: "",
+            email: "",
         };
     },
 
     created() {
-
-        this.cliente1()
-
-
+        this.cliente1();
     },
+
     methods: {
         clientes() {
             axios
                 .get("/api/clientes")
-                .then(respuesta => {
-                    this.clienteMelba = respuesta.data
-                    console.log(this.clienteMelba)
-                    this.cuentasMelba = this.clienteMelba.cuentas
-                    console.log(this.cuentasMelba)
+                .then((respuesta) => {
+                    // Haz algo con la respuesta si es necesario
                 })
-                .catch(error => {
+                .catch((error) => {
                     console.error('Error al obtener clientes:', error);
                 });
+        },
 
         cliente1() {
             axios
                 .get("/api/clientes/1")
-                .then(respuesta => {
-                    this.clienteMelba = respuesta.data
-                    console.log(this.clienteMelba)
-                    this.cuentasMelba = this.clienteMelba.cuentas
-                    console.log(this.cuentasMelba)
+                .then((respuesta) => {
+                    this.clienteMelba = respuesta.data;
+                    console.log(this.clienteMelba);
+                    this.cuentasMelba = this.clienteMelba.cuentas;
+                    console.log(this.cuentasMelba);
 
-                    this.name = this.clienteMelba.nombre
-                    this.lastName = this.clienteMelba.apellido
-                    this.email = this.clienteMelba.correo
+                    this.name = this.clienteMelba.nombre;
+                    this.lastName = this.clienteMelba.apellido;
+                    this.email = this.clienteMelba.correo;
                 })
-                .catch(error => {
+                .catch((error) => {
                     console.error('Error al obtener clientes:', error);
                 });
         },
 
-        mostrarDatosPersonales(){
-            this.visibleDatosPersonales = true
-            this.visibleCuentas = false
+        mostrarDatosPersonales() {
+            this.visibleDatosPersonales = true;
+            this.visibleCuentas = false;
         },
-        mostrarCuentas(){
-            this.visibleCuentas= true
-            this.visibleDatosPersonales = false
-
+        mostrarCuentas() {
+            this.visibleCuentas = true;
+            this.visibleDatosPersonales = false;
         },
 
-    logout() {
-        axios
-            .post("http://localhost:8080/api/logout")
-            .then((response) => {
-                console.log("signed out!!!");
-                location.href = "http://localhost:8080/web/index.html";
-            })
-            .catch((error) => console.log(error));
-    }
-
-}
-
+        logout() {
+            axios
+                .post("http://localhost:8080/api/logout")
+                .then((response) => {
+                    console.log("signed out!!!");
+                    location.href = "http://localhost:8080/web/index.html";
+                })
+                .catch((error) => console.log(error));
+        },
+    },
 }).mount('#app');
